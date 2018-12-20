59a5a839da7277b69b05ac7629382da43db1f230
Naram Kim a.k.a. Unknown <unknown@grotesq.com>
2018-12-20 21:38:08 +0900

    initial commit

---
+++ docs/precache-manifest.7695ce49cff23fe58938158c0112be55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "fa4af32c24293915c326",
+    "url": "/static/js/main.fa4af32c.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "fa4af32c24293915c326",
+    "url": "/static/css/main.0fbd8bee.chunk.css"
+  },
+  {
+    "revision": "db62388d78f0fa4639f729c262f0ab8f",
+    "url": "/index.html"
+  }
+];
